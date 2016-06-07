0a69f505ce16c4c9b3aa08fab8b12a0203dcca32
Orkad <nicolas.gidon.43@gmail.com>
2016-06-08 00:50:33 +0200

    list is now working

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
+/*
+ * Module dependencies
+ */
 var express = require('express');
 var body_parser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+//MongoDB link
 var URL = 'mongodb://localhost:27017/employeemanager';
 var app = express();
-var MongoClient = require('mongodb').MongoClient;
 
 app.use(express.static('public'));
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended: true}));
+
 app.set('view engine', 'jade');
 
+app.get('/', function(req,res){
+	res.render('index');
+});
+
+
+
 app.get('/collaborateurs', function (req, res) {
-   res.render('liste_employes');
-})
+	MongoClient.connect(URL, function(err, db) {
+		if(err)
+			res.send('Impossible de joindre la base de donnée');
+		db.collection('users').find({}).toArray(function(err,results){
+			res.render('liste_employes',{users : results});
+		});
+	});
+});
 
 app.get('/collaborateurs/new', function (req, res) {
 	res.render('nouvel_employe');
-})
+});
+
+app.get('/collaborateurs/watch', function (req, res) {
+	res.send('Ok');
+});
 
 app.post('/collaborateurs/new', function(req, res, next) {
    MongoClient.connect(URL, function(err, db) {
@@ -25,7 +47,6 @@ app.post('/collaborateurs/new', function(req, res, next) {
 		db.close();
 		res.send('Collaborateur ajouté avec succès');
 	});
-	
-})
+});
 
-var server = app.listen(80);
+app.listen(80);
