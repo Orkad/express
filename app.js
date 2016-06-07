630b6cf5b3f766d8b9b8a74cb24aa1ca6d8cddb8
GIDON Nicolas <nicholas43@hotmail.fr>
2016-06-07 14:36:09 +0200

    Mongo DB implementation (working)

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var body_parser = require('body-parser');
+var URL = 'mongodb://localhost:27017/employeemanager';
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
 
 app.use(express.static('public'));
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({
   extended: true
 })); 
+
+
+
+
+
 
 app.use(function (req, res, next) {
   console.log('Action at :', Date.now());
@@ -19,11 +26,18 @@ app.get('/collaborateurs', function (req, res) {
 
 app.get('/collaborateurs/new', function (req, res) {
    res.sendFile( __dirname + "/views/" + "nouvel_employe.html" );
+   
 })
 
 app.post('/collaborateurs/new', function(req, res, next) {
-   console.log(req.body);
-   res.send(req.body);
+   MongoClient.connect(URL, function(err, db) {
+		if(err)
+			res.send('Impossible de joindre la base de donnée');
+		db.collection('users').insert(req.body);
+		db.close();
+		res.send('Collaborateur ajouté avec succès');
+	});
+	
 })
 
 var server = app.listen(80);
